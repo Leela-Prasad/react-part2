eab41452aad1e85ecfda715ca289f00846ed77cc
Leela-Prasad <leela.jagu@gmail.com>
2024-08-19 22:48:44 +0530

    Custom Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { useContext, useReducer } from "react";
-import LoginStatus from "./state-management/components/LoginStatus";
-import taskReducer from "./state-management/reducers/taskReducer";
-import TasksContext from "./state-management/contexts/tasksContext";
-import TaskList from "./state-management/components/TaskList";
-import NavBar from "./state-management/components/NavBar";
+import { useReducer } from "react";
+import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/components/HomePage";
-import authReducer from "./state-management/reducers/authReducer";
-import { AuthContext } from "./state-management/contexts/authContext";
+import NavBar from "./state-management/components/NavBar";
+import TasksContext from "./state-management/contexts/tasksContext";
+import taskReducer from "./state-management/reducers/taskReducer";
 
 function App() {
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 

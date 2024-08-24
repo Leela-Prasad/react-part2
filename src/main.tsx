f9837789fd88cb06873c9564fdc26386e838cddf
Leela-Prasad <leela.jagu@gmail.com>
2024-08-24 22:02:40 +0530

    Setup Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,33 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./game-hub/theme";
 import { RouterProvider } from "react-router-dom";
-import router from "./routing/routes";
+import router from "./game-hub/routes";
+import theme from "./game-hub/theme";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  //   <QueryClientProvider client={queryClient}>
-  //     <ChakraProvider theme={theme}>
-  //       <ColorModeScript
-  //         initialColorMode={theme.config.initialColorMode}
-  //       ></ColorModeScript>
-  //       <RouterProvider router={router} />
-  //       <App />
-  //     </ChakraProvider>
-  //     <ReactQueryDevtools />
-  //   </QueryClientProvider>
-  // </React.StrictMode>
-
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ChakraProvider theme={theme}>
+        <ColorModeScript
+          initialColorMode={theme.config.initialColorMode}
+        ></ColorModeScript>
+        <RouterProvider router={router} />
+      </ChakraProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
+
+  // <React.StrictMode>
+  //   <QueryClientProvider client={queryClient}>
+  //     <RouterProvider router={router} />
+  //     <ReactQueryDevtools />
+  //   </QueryClientProvider>
+  // </React.StrictMode>
 );

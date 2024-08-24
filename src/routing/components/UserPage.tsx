6346c4f3e6e27523b6c047dda57e43ce184f7f3c
Leela-Prasad <leela.jagu@gmail.com>
2024-08-24 13:29:05 +0530

    Error Object and handling App, User Errors

--- src/routing/components/UserPage.tsx
+++ src/routing/components/UserPage.tsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import UserList from "./UserList";
 
 const UserPage = () => {
+  // throw new Error("Application Error");
+
   return (
     <div className="row">
       <div className="col">

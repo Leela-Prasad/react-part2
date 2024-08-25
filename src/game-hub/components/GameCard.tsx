09f30ac756a8608b001bf48b2b8eaa76878084fa
Leela-Prasad <leela.jagu@gmail.com>
2024-08-25 10:51:11 +0530

    Refactor Entities

--- src/game-hub/components/GameCard.tsx
+++ src/game-hub/components/GameCard.tsx
@@ -7,7 +7,7 @@ import {
   Text,
   transform,
 } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
